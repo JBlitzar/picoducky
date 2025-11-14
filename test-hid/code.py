73ce89192ae173ef724a9c00df06332aa1514b62
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2025-11-13 21:20:40 -0800

    Update code.py

--- test-hid/code.py
+++ test-hid/code.py
@@ -4,8 +4,19 @@ from adafruit_hid.keycode import Keycode
 from adafruit_hid.mouse import Mouse
 from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
 import board
+import time
 
 
 kbd = Keyboard(usb_hid.devices)
 layout = KeyboardLayoutUS(kbd)
-layout.write("hello world")
+kbd.press(Keycode.GUI, Keycode.SPACE)
+kbd.release_all()
+time.sleep(0.5)
+layout.write("terminal")
+time.sleep(0.5)
+kbd.press(Keycode.ENTER)
+kbd.release_all()
+time.sleep(0.5)
+layout.write("echo hello world")
+kbd.press(Keycode.ENTER)
+kbd.release_all()
